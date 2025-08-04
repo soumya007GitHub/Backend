8f28b1194c6cbf8de7ffc7f60a6bd54ae138727a
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-08-04 19:34:47 +0530

    Added 2nd folder with Diff. between commonJS(Old) & EcmaScript(Latest)

---
+++ 02/main.js
@@ -0,0 +1,10 @@
+// export const a = 10; //named export
+// export const b = 20; //named export
+// export const c = 30; //named export
+
+let obj;
+
+export default obj={
+    x: 10,
+    y:20
+}
---
+++ 02/server.js
@@ -0,0 +1,9 @@
+// import {a, b, c} from './main.js';
+
+// console.log(a, b, c);
+
+import anyName from './main.js';
+console.log(anyName);
+
+// const a = require("./myModule.js");
+// console.log(a);
